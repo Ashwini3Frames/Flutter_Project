654533665762c75b1ee1dc52bbc0e95ff012d820
Ashwini Madaraboina <ashwinimadaraboina@gmail.com>
2024-05-14 19:10:42 +0530

    working on home screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Your Application',
+      title: 'MY Application',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
